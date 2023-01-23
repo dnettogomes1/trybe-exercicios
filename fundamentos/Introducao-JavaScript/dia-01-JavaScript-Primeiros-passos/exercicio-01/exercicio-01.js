85911875c9dd898bc729405afa9c0aff07ef7337
Dinamerico Gomes neto <dnettogomes1@gmail.com>
2023-01-23 23:31:04 +0000

    exercicio-05

--- fundamentos/Introducao-JavaScript/dia-01-JavaScript-Primeiros-passos/exercicio-01/exercicio-01.js
+++ fundamentos/Introducao-JavaScript/dia-01-JavaScript-Primeiros-passos/exercicio-01/exercicio-01.js
@@ -88,3 +88,36 @@ switch (peca){
 
 }
 console.log(movimento)
+
+/*Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+
+Porcentagem >= 80 -> B
+
+Porcentagem >= 70 -> C
+
+Porcentagem >= 60 -> D
+
+Porcentagem >= 50 -> E
+
+Porcentagem < 50 -> F
+
+O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+*/
+
+const porcentagem = 180;
+if(porcentagem > 100 && porcentagem < 0){
+    console.log('ERRO')
+}else if(porcentagem >= 90){
+    console.log('A')
+}else if(porcentagem >= 80){
+    console.log('B')
+}else if(porcentagem >= 70){
+    console.log('C')
+}else if(porcentagem >=60){
+    console.log('D')
+}else if(porcentagem >=50){
+    console.log('E')
+}else{
+    console.log('F')
+}
