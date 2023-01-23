2158ccfbfe93a70304dac71ddcb0570c386a09fc
Dinamerico Gomes neto <dnettogomes1@gmail.com>
2023-01-23 21:40:53 +0000

    exercicio 03

--- fundamentos/Introducao-JavaScript/dia-01-JavaScript-Primeiros-passos/exercicio-01/exercicio-01.js
+++ fundamentos/Introducao-JavaScript/dia-01-JavaScript-Primeiros-passos/exercicio-01/exercicio-01.js
@@ -13,14 +13,24 @@ if (num1 > num2){
 
 /*Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três constantes com os valores que serão comparados.*/
 
-const numb1 = 10
-const numb2 = 20
+const numb1 = 100
+const numb2 = 60
 const numb3 = 30
 
-if (const numb1 > numb2 && numb1 > numb3){ 
+if (numb1 > numb2 && numb1 > numb3){ 
     console.log('O primeiro numero é o  maior entre os tres escritos.')
 }else if(numb2 > numb1 && numb2 > numb3){
     console.log('O segundo numero é maior entre os tres escritos.')
 }else{
-    console.log('O terceiro numero entre os tres escritos.')
+    console.log('O terceiro numero e o maior  entre os tres escritos.')
+}
+/*Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.*/
+
+const num = 10
+if (num > 0){
+    console.log("positive")
+}else if(num < 0){
+    console.log ("negative")
+}else{
+    console.log('zero')
 }
