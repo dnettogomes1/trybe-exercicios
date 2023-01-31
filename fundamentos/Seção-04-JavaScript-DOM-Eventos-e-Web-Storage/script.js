97907e42d55ce0c8994ad4e8be9f1d9c316dcc44
Dinamerico Gomes neto <dnettogomes1@gmail.com>
2023-01-31 19:02:18 +0000

    atualizaçao exercicio em horario de aula

---
+++ fundamentos/Seção-04-JavaScript-DOM-Eventos-e-Web-Storage/script.js
@@ -0,0 +1,20 @@
+/*const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+document.getElementById("page-title");
+
+/*Recupere o elemento que contém o título da página e faça algo com ele, por exemplo alterá-lo para o nome do seu filme favorito.*/
+document.getElementById("page-title").innerText = "Meu novo filme favorito"
+
+
+
+
+/*Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.*/
+
+document.getElementById("second-paragraph").innerText = "Estou aprendendo a usar o getElement para recuperar coisas e altera-las. "
+
+
+
+
+/*Por fim, recupere o subtítulo e altere-o também.*/
+document.getElementById("subtitle").innerText = "alteraçao do subtitulo concluida."
