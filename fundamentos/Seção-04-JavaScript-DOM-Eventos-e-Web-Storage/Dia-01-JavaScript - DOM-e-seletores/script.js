a7b1570dab8f448c18b0e59c87e6d6877100111a
Dinamerico Gomes neto <dnettogomes1@gmail.com>
2023-02-14 21:53:22 +0000

    atualizando minhas pastas

---
+++ fundamentos/Seção-04-JavaScript-DOM-Eventos-e-Web-Storage/Dia-01-JavaScript - DOM-e-seletores/script.js
@@ -0,0 +1,34 @@
+/*const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+document.getElementById("page-title");
+
+/* Recupere o elemento que contém o título da página e faça algo com ele, por exemplo alterá-lo para o nome do seu filme favorito. */
+document.getElementById("page-title").innerText = "Meu novo filme favorito"
+
+
+
+
+/* Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo. */
+
+//document.getElementById("second-paragraph").innerText = "Estou aprendendo a usar o getElement para recuperar coisas e altera-las. "
+
+
+
+
+/*Por fim, recupere o subtítulo e altere-o também.*/
+document.getElementById("subtitle").innerText = "alteraçao do subtitulo concluida."
+
+/* Adicione uma classe igual para os dois parágrafos;
+Recupere os seus parágrafos via código JavaScript, usando a função getElementsByClassName; */
+
+
+
+/* Altere algum estilo do primeiro deles; */
+
+let paragrafos = document.getElementsByClassName('paragraphs')
+for (index = 0; index < paragrafos.length; index += 1){
+      paragrafos[index].innerText = 'neto lindo'
+}
+ 
+/* Recupere o subtítulo e altere a cor dele usando a função getElementsByTagName.*/
